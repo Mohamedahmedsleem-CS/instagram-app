d4fcf29862dc6192add53b9e5bedbc51c75df966
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2023-06-16 23:19:20 +0300

    Organizing The Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+    root "home#index"
+
+  # root :to => "devise/sessions#new"
+#   devise_scope :user do
+#   authenticated :user do
+#     root 'home#index', as: :authenticated_root
+#   end
+
+#   unauthenticated do
+#     root 'devise/sessions#new', as: :unauthenticated_root
+#   end
+# end
+
 end
