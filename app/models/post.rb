0b9c961213d70ec53aac1d8ed3d62b0f456a1c36
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2023-10-20 01:59:58 +0300

    Edit Profile Page & enhancing the application

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,11 @@ class Post < ApplicationRecord
   validates :images, presence: true, blob: { content_type: :image } # supported options: :web_image, :image, :audio, :video, :text
   
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
+  #if we try to delete this post, ther is some likes and comments, which depends on this post.
+  # if we deleted the post then the dependdency is to destroy also the likes and destroy also the comments.
   has_many :likers, through: :likes, source: :user
   # the post has many likers through the likes model and give the user ID from the source of user model
   # (the  user which is inside the like model )
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
