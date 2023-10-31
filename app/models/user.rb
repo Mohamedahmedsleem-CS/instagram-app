5533cccdc82d70e14744df6f99ea9d4645499ee6
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2023-10-31 08:54:08 +0200

    add controllers follow system

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,10 +19,16 @@ class User < ApplicationRecord
 
   has_many :accepted_sent_requests, -> {where(accepted: true) }, class_name: "Follow", foreign_key: "follower_id"
 
+  has_many :waiting_sent_requests, -> {where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
+
+
   has_many :followers, through: :accepted_recieved_requests, source: :follower
   has_many :followings, through: :accepted_sent_requests, source: :followed
+  # has_many :waiting_followings, through: :waiting_sent_requests, source: :followed
+  has_many :waiting_followings, through: :waiting_sent_requests, source: :followed
+  # get all thes users that i am wating for
 
-  
+
   def follow(user)
     Follow.create(follower: self, followed: user)
   end
@@ -31,6 +37,9 @@ class User < ApplicationRecord
     self.accepted_sent_requests.find_by(followed: user)&.destroy
   end
 
+  def cancel_request(user)
+    self.waiting_sent_requests.find_by(followed: user)&.destroy
+  end
 
 
 
@@ -59,6 +68,5 @@ class User < ApplicationRecord
   # has_many :recieved_requests, class_name: "follow", foreign_key: "followed_id" # follow me 
   # has_many :sent_requests, class_name: "follow", foreign_key: "follower_id" # who i'm  following
 
-  # has_many :waiting_sent_requests, ->{where(accepted: false)}, class_name: "follow", foreign_key: "follower_id"
 
 end
